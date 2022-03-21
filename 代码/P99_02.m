33f3cbe284c8b1642b40faa21ce72260ac2877c2
fbanhua <3037537407@qq.com>
2022-03-21 21:18:58 +0800

    032101

--- 代码/P99_02.m
+++ 代码/P99_02.m
@@ -6,16 +6,24 @@ end
 for i=3:7
     A(i,1) = 1;
     for j=2:7
-        A(i,j) = (i-1)^(j);
+        A(i,j) = (i-1)^(j-1);
     end
 end
 B = [1;3;2;1;3;2;1];
 X = uptrbk(A,B);
 result = X';
+A;
+result_use = zeros(1,7);
+for i=1:7
+    result_use(1,i) = result(1,7-i+1);
+end
+result
+result_use
 X_draw = 0:0.1:6;
-Y_draw = polyval(result,X_draw);
+Y_draw = polyval(result_use,X_draw);
 plot(X_draw,Y_draw)
 xt = [0 1 2 3 4 5 6];
 yt = [1 3 2 1 3 2 1];
 str={'(0,1)','(1,3)','(2,2)','(3,1)','(4,3)','(5,2)','(6,1)'};
-text(xt,yt,str)
+text(xt,yt,'*','color','r')
+% text(xt,yt,str)
